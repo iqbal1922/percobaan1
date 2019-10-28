6bd5b9388d1b4d93ba02094721b54bdc61669c26
iqbal1922 <muhammadiqbaluddin1922@gmail.com>
2019-10-28 17:04:51 +0700

    Assalamualikum tahap.2

--- star/src/star/Star.java
+++ star/src/star/Star.java
@@ -17,6 +17,7 @@ public class Star {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
+        System.out.println("RUWET");
         
         System.out.print("Masukkan nilai N = ");
        int N = sc.nextInt();
